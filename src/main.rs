bdd3498517fe9dd22b35a002b186c30b7694f63d
Thomas Lacroix <toto.rigolo@free.fr>
2019-07-08 22:04:05 +0200

    Mount app to #pathbot-root

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 use pathbot::{Model, Msg};
 use yew::App;
+use stdweb::web::{document, IParentNode};
 
 fn main() {
     web_logger::init();
-
     yew::initialize();
 
+    let mount_point = document()
+        .query_selector("#pathbot-root")
+        .expect("can't find #pathbot-root node for mounting app")
+        .expect("can't unwrap #pathbot-root node");
+
     App::<Model>::new()
-        .mount_to_body()
+        .mount(mount_point)
         .send_message(Msg::Init);
 
     yew::run_loop();
